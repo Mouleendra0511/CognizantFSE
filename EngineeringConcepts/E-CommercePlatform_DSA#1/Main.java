3b676e709a747af6dec05e112d689a1c28eeb794
Aithi Aditya Sai Mouleendra <140081493+Mouleendra@users.noreply.github.com>
2025-06-19 17:06:29 +0530

    Update Main.java

--- EngineeringConcepts/E-CommercePlatform_DSA#1/Main.java
+++ EngineeringConcepts/E-CommercePlatform_DSA#1/Main.java
@@ -10,16 +10,12 @@ public class Main {
             new Product(103, "Phone", "Electronics"),
             new Product(104, "Notebook", "Stationery")
         };
-
-        // Sort for binary search
         Arrays.sort(products, Comparator.comparingInt(Product::getProductId));
-
         int searchId = 103;
-
         System.out.println("🔍 Linear Search:");
+        
         Product result1 = SearchUtility.linearSearch(products, searchId);
         System.out.println(result1 != null ? "Found: " + result1 : "Product not found");
-
         System.out.println("\n⚡ Binary Search:");
         Product result2 = SearchUtility.binarySearch(products, searchId);
         System.out.println(result2 != null ? "Found: " + result2 : "Product not found");
