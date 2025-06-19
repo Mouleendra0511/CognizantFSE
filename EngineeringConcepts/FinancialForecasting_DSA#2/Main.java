4c395526b20c62fae46d9416d756f7a40e999656
Aithi Aditya Sai Mouleendra <140081493+Mouleendra@users.noreply.github.com>
2025-06-19 22:38:42 +0530

    Update Main.java

--- EngineeringConcepts/FinancialForecasting_DSA#2/Main.java
+++ EngineeringConcepts/FinancialForecasting_DSA#2/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         double initialValue = 10000.0;
-        double growthRate = 0.08; // 8% growth rate
+        double growthRate = 0.08;
         int years = 5;
 
         FinancialForecast forecast = new FinancialForecast();
         OptimizedForecast optimized = new OptimizedForecast();
         System.out.println(" Recursive Forecast:");
-        System.out.printf("Value after %d years: ₹%.2f%n", years, forecast.forecastRecursive(initialValue, growthRate, years));
-        System.out.println("\n⚡ Optimized Recursive Forecast (Memoization):");
-        System.out.printf("Value after %d years: ₹%.2f%n",
+        System.out.printf("Value after %d years: Rs%.2f%n", years, forecast.forecastRecursive(initialValue, growthRate, years));
+        System.out.println("\n Optimized Recursive Forecast (Memoization):");
+        System.out.printf("Value after %d years: Rs%.2f%n",
                 years, optimized.forecastMemoized(initialValue, growthRate, years));
     }
 }
